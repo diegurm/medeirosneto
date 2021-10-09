267a6d6979ec3d224e841115c974d853cfb76d23
Diego Moreira <diego.moreira@n3urons.com>
2021-10-09 00:55:06 -0300

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7ac80a318b912f932f9450b307863ad'),
+    },
+  },
+});
